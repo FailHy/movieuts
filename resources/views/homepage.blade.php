94f4b8d5106521c1531cc7889d75d4af16a82878
FailHy <hylmikhalil@gmail.com>
2025-04-23 18:13:55 +0700

    menyesuaikan view dengan API

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,19 +10,22 @@
     </div>    
 @endif
 
-<h1>Popular Movie</h1>
+<h1>Popular Movies</h1>
 <div class="row">
     @foreach ($movies as $movie)
     <div class="col-lg-6">
         <div class="card mb-3" style="max-width: 540px;">
             <div class="row g-0">
-              <div class="col-md-4">
-                <img src="/images/{{ $movie['foto_sampul'] }}" class="img-fluid rounded-start" alt="...">
-            </div>
+                <div class="col-md-4">
+                    <img 
+                        src="{{ isset($movie['poster_path']) ? 'https://image.tmdb.org/t/p/w500' . $movie['poster_path'] : '/images/' . ($movie['foto_sampul'] ?? 'default.jpg') }}" 
+                        class="img-fluid rounded-start" 
+                        alt="{{ $movie['title'] ?? $movie['judul'] }}">
+                </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">{{ $movie['judul'] }}</h5>
-                        <p class="card-text">{{ $movie['sinopsis'] }}</p>
+                        <h5 class="card-title">{{ $movie['title'] ?? $movie['judul'] }}</h5>
+                        <p class="card-text">{{ $movie['overview'] ?? $movie['sinopsis'] }}</p>
                         <a href="/movie/{{ $movie['id'] }}" class="btn btn-success">Lihat Selanjutnya</a>
                     </div>
                 </div>
@@ -30,8 +33,5 @@
         </div>
     </div>
     @endforeach
-    <div class="d-flex justify-content-center">
-        {{ $movies->links() }}
-    </div>
 </div>
 @endsection
